926061edd7bd97a1730494a29185b6d09fc1a4ba
Arunas Dvilevicius <arunasdvi@gmail.com>
2013-03-14 10:25:41 +0200

    papildem aprasyma kaip naudotis sincos

--- sincos.m
+++ sincos.m
@@ -1,3 +1,6 @@
 function fsincos=sincos(x);
+% grazina funkcijos: f= sin(x)+ cos(x) reiksme
+% paleidziame : f=sincos(x), kur x vienmatis vektorius
+% pvz.: sincos(1.0);
 fsincos=sin(x)+cos(x);
 end
